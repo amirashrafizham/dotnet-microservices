b0b14904f6f9b8672d0fbf488a65c9ff17d7a8cb
Amir Ashraf Izham <amirashrafizham@gmail.com>
2023-01-21 10:26:42 +0800

    created PlatformRepo

---
+++ PlatformService/Data/IPlatformRepo.cs
@@ -0,0 +1,12 @@
+using System;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public interface IPlatformRepo
+    {
+        IEnumerable<Platform> GetAllPlatforms();
+        Platform GetPlatformById(int id);
+        void CreatePlatform(Platform plat);
+    }
+}
